6e28c776d43c592991f53c39283b35017b0216f6
Evan Relf <evan@evanrelf.com>
2022-11-23 21:58:49 -0800

    `iced` hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Element, Sandbox, Settings};
+
+fn main() -> Result<(), iced::Error> {
+    ShaiHulud::run(Settings::default())
+}
+
+struct ShaiHulud {}
+
+#[derive(Debug)]
+enum Message {}
+
+impl iced::Sandbox for ShaiHulud {
+    type Message = Message;
+
+    fn new() -> Self {
+        ShaiHulud {}
+    }
+
+    fn title(&self) -> String {
+        String::from("Shai-Hulud")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        iced::widget::text("Hello, world!").into()
+    }
 }
